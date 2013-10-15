99ee7bb06f668b5ac85af9aebfc0a7c27e766b0a
NachE <nache.nache@gmail.com>
2013-10-15 19:35:41 +0200

    improving code while I read node.js api

--- http_stream.js
+++ http_stream.js
@@ -22,36 +22,50 @@ http.createServer(function(req, res) {
 	var stats;
 	console.log("File req: " + filename);
 	
-	try {
-		stats = fs.lstatSync(filename);
-	} catch (e) {
-		res.writeHead(404, {'Content-Type': mimeTypes['txt']});
-		res.write('HTTP 404 - File Not Found');
-		res.end();
-		return;
-	}
-  
-	if (stats.isFile()) {
+	//
+	fs.lstat(filename, function(err,stats){
 	
-		var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+		if ( !(typeof stats === "undefined") && stats.isFile()) {
+	
+			var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
 		
-		if (typeof mimeType === "undefined")
-			mimeType = mimeTypes["*"];
+			if (typeof mimeType === "undefined")
+				mimeType = mimeTypes["*"];
 			
-		res.writeHead(200, {'Content-Type': mimeType} );
-		// From API DOC:
-		//
-		// An example to read the last 10 bytes of a file which is 100 bytes long:
-		// fs.createReadStream('sample.txt', {start: 90, end: 99});
-		//
-		// If autoClose is set to true (default behavior), on error or end the 
-		// file descriptor will be closed automatically.
-		var fileStream = fs.createReadStream(filename, {
-							'bufferSize': 4 * 1024,
-							'autoClose': true
-						 }).pipe(res);
-		//res.end(); not needed, autoClose do it
-		//return;
-	}
+			res.writeHead(200, {'Content-Type': mimeType} );
+			// From API DOC:
+			//
+			// An example to read the last 10 bytes of a file which is 100 bytes long:
+			// fs.createReadStream('sample.txt', {start: 90, end: 99});
+			//
+			// If autoClose is set to true (default behavior), on error or end the 
+			// file descriptor will be closed automatically.
+			//
+			// I have been see 'bufferSize': 4 * 1024 in some code, see later
+			// autoClose is default to true, but I like to sure
+			var fileStream = fs.createReadStream(filename, {
+								'autoClose': true
+							}).pipe(res);
+			//res.end(); not needed, autoClose do it
+			//return;
+			
+		
+		}else if( err.errno == 34 ){
+		//if no error, err.errno = null
+		
+			res.writeHead(404, {'Content-Type': mimeTypes['txt']});
+			res.write('HTTP 404 - File Not Found');
+			res.end();
+			return;
+		
+		}else{
+		
+			res.writeHead(500, {'Content-Type': mimeTypes['txt']});
+			res.write('HTTP 500 - Internal Server Error');
+			res.end();
+			return;
+			
+		}
+	});
 	
 }).listen(8901);
